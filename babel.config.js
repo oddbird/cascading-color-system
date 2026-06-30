0a0e1be3cd2a45aae8c749f67ffb8376ee32798d
Jonny Gerig Meyer <jonny@oddbird.net>
2026-06-30 11:45:07 -0400

    upgrade deps

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  plugins: ["@babel/plugin-transform-modules-umd"],
-  moduleId: 'ccs',
+  plugins: [
+    ["@babel/plugin-transform-modules-umd", {
+      moduleId: 'ccs',
+    }],
+  ],
 };
